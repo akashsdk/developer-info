c084bd9f693f91cd9eb757a236a86aa5a9ab81f9
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-04 19:09:26 +0600

    ok

--- src/Screen/index.js
+++ src/Screen/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import '../Style/index.css'
 
 export default function index() {
   return (
     <div>
-      <h1>Welcome to Developer Info. & contact Page</h1>
+      <h1 className="indexH1">Welcome to Developer Info. & contact Page</h1>
     </div>
   );
 }
