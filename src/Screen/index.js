a98114ba8afe546d00616aee476cfa774bbeb0a2
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-07 17:45:38 +0600

    ok

--- src/Screen/index.js
+++ src/Screen/index.js
@@ -1,10 +1,22 @@
 import React from "react";
-import '../Style/index.css'
+import "../Style/index.css";
 
 export default function index() {
   return (
     <div>
       <h1 className="indexH1">Welcome to Developer Info. & contact Page</h1>
+      <div>
+      <h3 className="indexH3">hi! There, i'm Tasnim Shahriar Akash...</h3>
+      </div>
+      {/* <div>
+        <video autoplay muted loop>
+          <source
+            src="https://mdbootstrap.com/img/video/Sail-Away.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <h2 className="indexH2">sail</h2>
+      </div> */}
     </div>
   );
 }
