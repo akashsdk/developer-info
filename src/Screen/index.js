ca0a44ac8b73c0204d7ae1105412c26f789d4431
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-07 18:09:47 +0600

    ok

--- src/Screen/index.js
+++ src/Screen/index.js
@@ -3,13 +3,13 @@ import "../Style/index.css";
 import { Col, Row, Layout, Button, Radio } from "antd";
 import img from "../Image/on-desk.png";
 import {
-    GithubOutlined,
-    FacebookOutlined,
-    LinkedinOutlined,
-    GoogleOutlined,
-    TwitterOutlined,
-    WhatsAppOutlined,
-  } from "@ant-design/icons";
+  GithubOutlined,
+  FacebookOutlined,
+  LinkedinOutlined,
+  GoogleOutlined,
+  TwitterOutlined,
+  WhatsAppOutlined,
+} from "@ant-design/icons";
 
 export default function index() {
   return (
@@ -27,63 +27,65 @@ export default function index() {
         </video>
         <h2 className="indexH2">sail</h2>
       </div> */}
-      
-      <div>gg</div>
 
-      <Layout className="HomeLayout">
-        <div style={{ flex: 1, flexDirection: "row" }}>
-          <h1 style={{ color: "aqua" }}>me.!</h1>
-          <p>A passionate Full Stack Software Developer,</p>
-          <p> having an experience of building Web and Mobile applications </p>
-          <p>with JavaScript / Reactjs / React Native / Nodejs and </p>
-          <p>other cool libraries and frameworks</p>
-          <div className="HomeLayoutLine"></div>
-          <div>
-            <Button
-              type="link"
-              className="HomeLayoutButton"
-              href="https://github.com/akashsdk"
-            >
-              <GithubOutlined className="HomeLayoutIcon" />
-            </Button>
+      <div>
+        <Layout className="HomeLayout">
+          <div style={{ flex: 1, flexDirection: "row" }}>
+            <h1 style={{ color: "aqua" }}>me.!</h1>
+            <p style={{color:'#fff'}}>A passionate Full Stack Software Developer,</p>
+            <p>
+              {" "}
+              having an experience of building Web and Mobile applications{" "}
+            </p>
+            <p>with JavaScript / Reactjs / React Native / Nodejs and </p>
+            <p>other cool libraries and frameworks</p>
+            <div className="HomeLayoutLine"></div>
+            <div>
+              <Button
+                type="link"
+                className="HomeLayoutButton"
+                href="https://github.com/akashsdk"
+              >
+                <GithubOutlined className="HomeLayoutIcon" />
+              </Button>
 
-            <Button
-              type="link"
-              className="HomeLayoutButton"
-              href="https://www.linkedin.com/in/tasnimsakash/"
-            >
-              <LinkedinOutlined className="HomeLayoutIcon" />
-            </Button>
+              <Button
+                type="link"
+                className="HomeLayoutButton"
+                href="https://www.linkedin.com/in/tasnimsakash/"
+              >
+                <LinkedinOutlined className="HomeLayoutIcon" />
+              </Button>
 
-            <Button type="link" className="HomeLayoutButton" href="">
-              <GoogleOutlined className="HomeLayoutIcon" />
-            </Button>
+              <Button type="link" className="HomeLayoutButton" href="">
+                <GoogleOutlined className="HomeLayoutIcon" />
+              </Button>
 
-            <Button
-              type="link"
-              className="HomeLayoutButton"
-              href="https://www.facebook.com/tasnimSakash/"
-            >
-              <FacebookOutlined className="HomeLayoutIcon" />
-            </Button>
+              <Button
+                type="link"
+                className="HomeLayoutButton"
+                href="https://www.facebook.com/tasnimSakash/"
+              >
+                <FacebookOutlined className="HomeLayoutIcon" />
+              </Button>
 
-            <Button
-              type="link"
-              className="HomeLayoutButton"
-              href="https://twitter.com/tasnimSakash"
-            >
-              <TwitterOutlined className="HomeLayoutIcon" />
-            </Button>
+              <Button
+                type="link"
+                className="HomeLayoutButton"
+                href="https://twitter.com/tasnimSakash"
+              >
+                <TwitterOutlined className="HomeLayoutIcon" />
+              </Button>
 
-            <Button
-              type="link"
-              className="HomeLayoutButton"
-              href="https://api.whatsapp.com/send?phone=8801580233165&text=hello%F0%9F%98%87"
-            >
-              <WhatsAppOutlined className="HomeLayoutIcon" />
-            </Button>
-          </div>
-          {/* <div>
+              <Button
+                type="link"
+                className="HomeLayoutButton"
+                href="https://api.whatsapp.com/send?phone=8801580233165&text=hello%F0%9F%98%87"
+              >
+                <WhatsAppOutlined className="HomeLayoutIcon" />
+              </Button>
+            </div>
+            {/* <div>
             <Link className="HomeLayoutButton2" to="/ContactMe">
               Contact Me
             </Link>
@@ -91,11 +93,12 @@ export default function index() {
               SEE MY RESUME
             </a>
           </div> */}
-        </div>
-        <div style={{ flex: 1 }}>
-          <img src={img} className="HomeLayoutImg"></img>
-        </div>
-      </Layout>
+          </div>
+          <div style={{ flex: 1 }}>
+            <img src={img} className="HomeLayoutImg"></img>
+          </div>
+        </Layout>
+      </div>
     </div>
   );
 }
