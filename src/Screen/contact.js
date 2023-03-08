a7816dbaae046daa8a1ebea73052b814438f37d0
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-08 12:19:28 +0600

    ok

--- src/Screen/contact.js
+++ src/Screen/contact.js
@@ -38,6 +38,7 @@ export default function contact() {
           {...layout}
           name="nest-messages"
           onFinish={onFinish}
+          className="contactFrom"
           style={{
             maxWidth: '80%',
           }}
