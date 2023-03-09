14b1df967b5e944de938d37104c096ed3d000332
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-09 21:26:53 +0600

    ok

--- src/Screen/home.js
+++ src/Screen/home.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import '../Style/home.css';
-import Index from './index';
-import Contact from './contact';
+import React from "react";
+import "../Style/home.css";
+import Index from "./index";
+import Contact from "./contact";
 
 export default function home() {
   return (
     <div>
-      <Index/>
-      <div style={{height:'60px'}}/>
-      <Contact/>
-      <div style={{height:'60px'}}/>
+      <Index />
+      <div style={{ height: "60px" }} />
+      <Contact />
+      <div style={{ height: "40px" }} />
+      <div className="homeDownText">
+        <h3>
+          “The hour of departure has arrived, and we go our separate ways, I to
+          die, and you to live. Which of these two is better only God knows.”
+        </h3>
+      </div>
+      <div style={{ height: "30px" }} />
     </div>
-  )
+  );
 }
