be5d439f86b70a16524031824961d7c98cda616e
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-09 21:34:58 +0600

    ok

--- src/Screen/home.js
+++ src/Screen/home.js
@@ -9,11 +9,13 @@ export default function home() {
       <Index />
       <div style={{ height: "60px" }} />
       <Contact />
-      <div style={{ height: "40px" }} />
+      <div style={{ height: "60px" }} />
       <div className="homeDownText">
-        <h3>
-          “The hour of departure has arrived, and we go our separate ways, I to
-          die, and you to live. Which of these two is better only God knows.”
+        <h3 className="homeDownH3">
+          “The hour of departure has arrived, <br/>
+          and we go our separate ways, <br/>
+          I to die, and you to live. <br/>
+          Which of these two is better only God knows.” <br/> <br/>― Socrates
         </h3>
       </div>
       <div style={{ height: "30px" }} />
