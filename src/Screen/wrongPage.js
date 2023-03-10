6f6c5b780748178485743184803fe7b2964e0520
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-10 15:51:29 +0600

    Update wrongPage.js

--- src/Screen/wrongPage.js
+++ src/Screen/wrongPage.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Button, Result } from "antd";
 
 export default function wrongPage() {
   return (
-    <div>wrongPage</div>
-  )
+    <div style={{height:'800px',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    }}>
+      <div >
+        <Result
+          status="500"
+          title="500"
+          subTitle="Sorry, something went wrong."
+          extra={<Button type="primary">Back Home</Button>}
+        />
+      </div>
+    </div>
+  );
 }
