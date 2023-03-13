5510212b7da51f964634d9a711ee89b0c0762d2b
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-13 11:44:48 +0600

    Update header.js

--- src/Component/header.js
+++ src/Component/header.js
@@ -5,7 +5,7 @@ export default function header() {
   return (
     <div className='headerBody'>
         <div className='headerBox'>
-            <h2 style={{color:'#fff'}}>Welcome</h2>
+            <h2 >Welcome</h2>
         </div>
     </div>
   )
