e3da97892dd25ec462ba2af3564762cec18891ad
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-13 11:40:28 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Screen/home"
 import WrongPage from "./Screen/wrongPage";
+import Login from "./Screen/loin";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
         <Route path="*" element={<WrongPage />} />
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
