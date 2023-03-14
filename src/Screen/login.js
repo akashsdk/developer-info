448166623fe58c8ff0d18726b4f37139bfef66d0
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-14 12:28:09 +0600

    Update login.js

--- src/Screen/login.js
+++ src/Screen/login.js
@@ -1,19 +1,84 @@
-import React from 'react'
+import React from "react";
 import "../Style/login.css";
-import { Button, Checkbox, Form, Input } from 'antd';
-
+import { Button, Checkbox, Form, Input } from "antd";
 
 const onFinish = (values) => {
-  console.log('Success:', values);
+  console.log("Success:", values);
 };
 const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
+  console.log("Failed:", errorInfo);
 };
 
 export default function login() {
   return (
-    <div className='loginBody'>
+    <div className="loginBody">
+      
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
-  )
+  );
 }
