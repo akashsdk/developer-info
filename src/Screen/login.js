1b9c5e24801c646128d9d67c7e6667fae9aa396a
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-14 12:27:05 +0600

    Update login.js

--- src/Screen/login.js
+++ src/Screen/login.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import "../Style/login.css";
+import { Button, Checkbox, Form, Input } from 'antd';
+
+
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
 
 export default function login() {
   return (
     <div className='loginBody'>
-      
+
     </div>
   )
 }
