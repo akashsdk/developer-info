d8dd5b3be962c66aaf292a37beda5bee69c62891
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-21 01:00:12 +0600

    ok

--- src/Component/header.js
+++ src/Component/header.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import "./header.css";
 
 export default function header() {
   return (
-    <div className='headerBody'>
-        <div className='headerBox'>
-            <h2 >Welcome </h2>
-        </div>
+    <div className="headerBody">
+      <div className="headerBox" >
+        <h2 className="headerText" >
+          <span>I</span>M<span >POSSIBLE</span>
+        </h2>
+      </div>
     </div>
-  )
+  );
 }
