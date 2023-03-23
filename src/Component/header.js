db60158d2b73bcb4dcebfd33161bd9292bb831ce
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-23 18:42:31 +0600

    ok

--- src/Component/header.js
+++ src/Component/header.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./header.css";
-import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+import { Button, Space, Tooltip } from "antd";
 
 export default function header() {
   return (
     <div className="headerBody">
-      <div className="headerBox" >
-        <h4 className="headerText" >
-          <span>I</span>M<span >AKASH</span>
+      <div className="headerBox">
+        <h4 className="headerText">
+          <span>I</span>M<span>AKASH</span>
         </h4>
-        <div className="headerIconDiv">
-        <ExclamationCircleOutlined className="headerIcon"/>
-        </div>
+        <Space wrap>
+          <Tooltip title="for admin!!" className="headerIconDiv">
+            <Button
+              type="dashed"
+              shape="circle"
+              icon={<ExclamationCircleOutlined className="headerIcon" />}
+            />
+          </Tooltip>
+        </Space>
       </div>
-      
     </div>
   );
 }
