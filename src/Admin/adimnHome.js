1f21095fb74852938e548dadcd3e35aa129f61cf
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-26 03:41:53 +0600

    Update adimnHome.js

--- src/Admin/adimnHome.js
+++ src/Admin/adimnHome.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./adimnHome.css";
 import { Link } from "react-router-dom";
-import { LogoutOutlined } from '@ant-design/icons';
+import { LogoutOutlined } from "@ant-design/icons";
 
 export default function adimnHome() {
   return (
     <div className="adminHomeBody">
       <div className="adminHomeHDiv">
-      <h1 className="adminHomeHtext">
-        <span>Ad</span>min<span>Dashboard</span>
-      </h1>
-      <Link to="/" className="adminHomeLink">
-        <LogoutOutlined className="adminHomeIcon"/>
+        <h1 className="adminHomeHtext">
+          <span>Ad</span>min<span>Dashboard</span>
+        </h1>
+        <Link to="/" className="adminHomeLink">
+          <LogoutOutlined className="adminHomeIcon" />
         </Link>
       </div>
       <div className="adminHomeLine" />
