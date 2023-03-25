e0abddfd2a32e37c99b14d01ec4c9d1118b3fd00
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-26 02:57:54 +0600

    ok

--- src/Admin/adimnHome.js
+++ src/Admin/adimnHome.js
@@ -4,9 +4,7 @@ import "./adimnHome.css";
 export default function adimnHome() {
   return (
     <div className='adminHomeBody'>
-      <div>
-        <h1>admin dashboard</h1>
-      </div>
+        <h1 className='adminHomeHtext'>Admin Dashboard</h1>
       <div>gg</div>
       <div>gg</div>
       <div>gg</div>
