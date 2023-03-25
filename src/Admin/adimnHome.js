88a3f58caee1ba84bc58f8f734d3344df3ba1dd7
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-26 03:29:18 +0600

    ok

--- src/Admin/adimnHome.js
+++ src/Admin/adimnHome.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./adimnHome.css";
+import { Link } from "react-router-dom";
+import { LogoutOutlined } from '@ant-design/icons';
 
 export default function adimnHome() {
   return (
-    <div className='adminHomeBody'>
-        <h1 className='adminHomeHtext'>Admin Dashboard</h1>
-      <div>gg</div>
+    <div className="adminHomeBody">
+      <div className="adminHomeHDiv">
+      <h1 className="adminHomeHtext">
+        <span>Ad</span>min<span>Dashboard</span>
+      </h1>
+      <div>
+      <Link to="/" className="adminHomeLink">
+        <LogoutOutlined className="adminHomeIcon"/>
+        </Link>
+      </div>
+      </div>
+      <div className="adminHomeLine" />
       <div>gg</div>
       <div>gg</div>
     </div>
-  )
+  );
 }
