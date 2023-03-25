e3fd5a961c1e89e2bce87268caa06e4917fcf13f
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-25 15:23:21 +0600

    Update adimnHome.js

--- src/Admin/adimnHome.js
+++ src/Admin/adimnHome.js
@@ -4,13 +4,7 @@ import "./adimnHome.css";
 export default function adimnHome() {
   return (
     <div>
-      adimnHome dd
-      tasnikhj
-      dfdfdfdf  dh
-      ghgjhjk bfgg
-
-      jh hddhd h hdbchjd 
-      
+      adimnHome
     </div>
   )
 }
