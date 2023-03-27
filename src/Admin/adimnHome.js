d5ad62f0f0b4ec220e8dad2d4d9430a4b688e295
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-03-27 13:59:33 +0600

    ok

--- src/Admin/adimnHome.js
+++ src/Admin/adimnHome.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./adimnHome.css";
 import { Link } from "react-router-dom";
 import { LogoutOutlined } from "@ant-design/icons";
-import { Pagination } from 'antd';
+import { Pagination } from "antd";
 
 export default function adimnHome() {
   return (
@@ -17,42 +17,41 @@ export default function adimnHome() {
       </div>
       <div className="adminHomeLine" />
       <div className="adminHomeDiv">
+        {/* Name */}
         <div className="adminHomeColumnDiv">
-        <p className="adminHomeColumnP">Name: </p>
-        <p className="adminHomeColumnSceP">Akash</p>
+          <p className="adminHomeColumnP">Name: </p>
+          <p className="adminHomeColumnSceP">Akash</p>
+        </div>
+        {/* Email */}
+        <div className="adminHomeColumnDiv">
+          <p className="adminHomeColumnP1">Email: </p>
+          <p className="adminHomeColumnSceP1">akash.tasnim@gmail.com</p>
+        </div>
+        {/* Age */}
+        <div className="adminHomeColumnDiv">
+          <p className="adminHomeColumnP1">Age: </p>
+          <p className="adminHomeColumnSceP1">20</p>
+        </div>
+        {/* Phone */}
+        <div className="adminHomeColumnDiv">
+          <p className="adminHomeColumnP1">Phone: </p>
+          <p className="adminHomeColumnSceP1">01580233165</p>
+        </div>
+        {/* Other's Link */}
+        <div className="adminHomeColumnDiv">
+          <p className="adminHomeColumnP1">Other's Link: </p>
+          <p className="adminHomeColumnSceP1">https://akash.www.com</p>
+        </div>
+        {/* Message */}
+        <div className="adminHomeColumnDiv">
+          <p className="adminHomeColumnP1">Message: </p>
+          <p className="adminHomeColumnSceP1">I there, i am akash.</p>
         </div>
 
-        <div className="adminHomeColumnDiv">
-        <p className="adminHomeColumnP1">Email: </p>
-        <p className="adminHomeColumnSceP1">akash.tasnim@gmail.com</p>
-        </div>
-
-        <div className="adminHomeColumnDiv">
-        <p className="adminHomeColumnP1">Age: </p>
-        <p className="adminHomeColumnSceP1">20</p>
-        </div>
-
-        <div className="adminHomeColumnDiv">
-        <p className="adminHomeColumnP1">Phone: </p>
-        <p className="adminHomeColumnSceP1">01580233165</p>
-        </div>
-
-        <div className="adminHomeColumnDiv">
-        <p className="adminHomeColumnP1">Other's Link: </p>
-        <p className="adminHomeColumnSceP1">https://akash.www.com</p>
-        </div>
-        
-        <div className="adminHomeColumnDiv">
-        <p className="adminHomeColumnP1">Message: </p>
-        <p className="adminHomeColumnSceP1">I there, i am akash.</p>
-        </div>
-
-        <div style={{height:'30px'}}/>
-        
+        <div style={{ height: "30px" }} />
       </div>
-      <div style={{height:'20px'}}/>
-      <Pagination defaultCurrent={1} total={50} />;
-      <div>gg</div>
+      <div style={{ height: "20px" }} />
+      <Pagination defaultCurrent={1} total={50} />;<div>gg</div>
     </div>
   );
 }
