7efb2a17a744c46e2a9e74f59a57ba8d91813c50
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-04-04 03:34:31 +0600

    Update Try.js

--- src/Admin/Try.js
+++ src/Admin/Try.js
@@ -1,35 +1,7 @@
-// import React, { useState, useEffect } from "react";
+import React from 'react'
 
-
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
-
-// import { getFirestore } from "firebase/firestore";
-
-// function Try() {
-//     const db = getFirestore(firestore);
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch the data from Firestore
-//     db.collection("contact")
-//       .get()
-//       .then((querySnapshot) => {
-//         const data = querySnapshot.docs.map((doc) => doc.data());
-//         setData(data);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Data from Firestore</h1>
-//       <ul>
-//         {data.map((item, index) => (
-//           <li key={index}>{item.name}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default Try;
+export default function Try() {
+  return (
+    <div>Try</div>
+  )
+}
