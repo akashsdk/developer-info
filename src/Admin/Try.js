6e1c420ef3d1d34fc2fda040e9ad94a50412027a
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-04-07 02:22:23 +0600

    Update Try.js

--- src/Admin/Try.js
+++ src/Admin/Try.js
@@ -3,15 +3,7 @@ import React from 'react'
 export default function Try() {
   return (
     <div>
-        <div>Try thst </div>
-        <div>Try thst </div>
-        <div>Try thst </div>
-        <div>Try thst </div>
-        <div>Try thst </div>
-        <div>Try thst </div>
-        <div>Try thst </div>
-        <div>Try thst </div>
-        <div>Try thst </div>
+        try
          </div>
   )
 }
