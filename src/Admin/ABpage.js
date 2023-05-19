f1f6d143844db85f4b257c4f65b1924bcdbb5d6d
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-05-19 22:05:28 +0600

    Update ABpage.js

--- src/Admin/ABpage.js
+++ src/Admin/ABpage.js
@@ -4,7 +4,7 @@ import './ABpage.css';
 export default function ABpage() {
   return (
     <div className='aBpageBody'>
-        <h1> 16 May 2013, Akash and Bristy </h1>
+        <h1> 16 May 2013, Akash and Bristy bcjxbc</h1>
     </div>
   )
 }
