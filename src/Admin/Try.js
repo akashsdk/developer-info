2d9b0e81aae3b401f7ddde1a5f2336463e4eba05
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-06-01 23:13:15 +0600

    ok

--- src/Admin/Try.js
+++ src/Admin/Try.js
@@ -1,5 +1,14 @@
 import React from "react";
+//  import  firebase from '../firebase';
 
 export default function Try() {
-  return <div>try</div>;
+
+  //  const ref = firebase.firestore().collection("contact")
+  //  console.log(ref);
+  
+
+  
+  return <div>
+    <div>hhh</div>
+  </div>;
 }
